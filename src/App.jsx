fda5fce715145238b3ffbea324c5802f7b3093f7
unknown <apurbahasanj@gmail.com>
2023-04-10 05:16:42 +0600

    error page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      {/* this is App */}
+      <Header />
+      <Outlet />
+      {/* Footer */}
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            This is a header
+        </div>
+    );
+};
+
+export default Header;
